fae79b39d7e5cbfc2240922762abd74e1d0692fb
Vaibhav Nagda <vaibhavnagda22@gmail.com>
2023-06-17 13:05:45 +0100

    Add dummydata, useState & useEffect hook
    react hooks
    - can be used only in function component and not in class component because class component have their own way to do things
    - must execute in same order e.g if they are around in if check (that means may or may not run) & so cannot be put around in conditional logic
    
    useState
    - call the function and pass in default value
    - returns array of values (usually only two value) So, de-structure those.
    1st value in array is always the current thing
    2nd value is a function which allows to update the current state
    
    useEffect
    Whenever options inside the array (The second parameter) change, that's when the logic inside useEffect is run

--- app/views/components/Category/CategoryList.js
+++ app/views/components/Category/CategoryList.js
@@ -1,4 +1,96 @@
-function CategoryList({ navigation }) {
+import { useEffect, useState } from "react";
 
+function CategoryList({ navigation }) {
+    const [categories, setCategories] = useState([]);
+    
+    function getCategories() {
+        return [
+          {
+            name: "Dry fruits",
+            createdDate: "1682559945",
+            modifiedDate: "1682559945",
+          },
+          {
+            name: "Spices",
+            createdDate: "1682559967",
+            modifiedDate: "1682559967",
+          },
+          {
+            name: "Pulses, Grains & Lentils & something",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Snacks",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Cleaning",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Chocolate",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Fruits",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Vegetables",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Beauty",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Pharmacy",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Toiletries",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Utensils",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Beauty 2",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Pharmacy 2",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Toiletries 2",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+          {
+            name: "Utensils 2",
+            createdDate: "1686759945",
+            modifiedDate: "1686759945",
+          },
+        ];
+    }
+
+    useEffect(() => {
+        const list = getCategories();
+        setCategories(list);
+    }, []);
 }
 export default CategoryList;
